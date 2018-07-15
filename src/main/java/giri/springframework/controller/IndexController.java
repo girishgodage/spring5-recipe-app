87301adbf091c7f5e54703a0ee0be846be43c4b6
girishgodage <girishgodage@gmail.com>
2018-07-15 07:06:27 +0530

    refactoring for project lombok and Add Transactional

--- src/main/java/giri/springframework/controller/IndexController.java
+++ src/main/java/giri/springframework/controller/IndexController.java
@@ -2,10 +2,12 @@ package giri.springframework.controller;
 
 
 import giri.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -17,10 +19,9 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
-
+        log.debug("Getting Index Page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
-
-        return "index";
+            return "index";
     }
 }
