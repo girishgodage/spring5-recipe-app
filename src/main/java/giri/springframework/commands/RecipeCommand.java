e18400f9d2f68a2b480fff1295f7d423717e22cf
girishgodage <girishgodage@gmail.com>
2018-07-17 19:05:20 +0530

    add Validation to Recipe form

--- src/main/java/giri/springframework/commands/RecipeCommand.java
+++ src/main/java/giri/springframework/commands/RecipeCommand.java
@@ -4,9 +4,11 @@ import giri.springframework.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.validation.constraints.*;
 
 /**
  * Created by jt on 6/21/17.
@@ -16,13 +18,30 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
